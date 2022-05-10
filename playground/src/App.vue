cff014c403d8ad85a741c8fbbac5db3af74fc16e
Jake Esser <jake@jakes-co.de>
2022-05-10 18:55:37 +0200

    Added playground

---
+++ playground/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+    <b-container>
+        <h1 class="text-center mb-5">Playground</h1>
+        <h2>Buttons</h2>
+        <div v-for="(variant, i) in ['primary', 'secondary', 'success', 'info', 'warning', 'danger']" :key="i" class="my-1">
+            <b-btn v-for="size in ['sm', 'md', 'lg']" :key="size" :variant="variant" :size="size" class="mx-1">b-btn variant="{{variant}}" size="{{size}}"</b-btn>
+        </div>
+        <h2>Badges</h2>
+        <div v-for="rounded in [false, true]" :key="rounded" class="my-1">
+            <b-badge v-for="variant in ['primary', 'secondary', 'success', 'info', 'warning', 'danger']" :key="variant" :variant="variant" :rounded="rounded" class="mx-1">b-badge variant="{{variant}}" rounded="{{ rounded }}"</b-badge>
+        </div>
+        <h2>Alerts</h2>
+        <b-alert v-for="variant in ['primary', 'secondary', 'success', 'info', 'warning', 'danger']" :key="variant" :variant="variant" class="mx-1">b-alert variant="{{variant}}"</b-alert>
+        <h2>Table</h2>
+        <b-table :headers="[
+            { text: '#', key: 'id' },
+            { text: 'First Name', key: 'first_name' },
+            { text: 'Last Name', key: 'last_name' },
+            { text: 'Actions', key: 'actions' }
+        ]" :items="[
+            { id: 1, first_name: 'Bob', last_name: 'Baker', variant: 'danger' },
+            { id: 2, first_name: 'Alice', last_name: 'Adams' }
+        ]">
+            <template v-slot:[`item.actions`]="{item}">
+                <b-btn @click="log(item)" variant="danger" size="sm">Delete</b-btn>
+            </template>
+        </b-table>
+        <h2>Form</h2>
+        <b-input>
+            Label
+        </b-input>
+        <b-check>Test</b-check>
+        <b-select :options="[{ text: 'Gruppe', $options: [ { text: 'A', value: 'Kein A' }, 'B' ] }, 'C']" />
+        <b-range />
+        <h2>Toast</h2>
+        <b-toast>
+            <template v-slot:header>
+                <b>Test</b>
+            </template>
+            Hallo Welt
+        </b-toast>
+        <h2>Modals</h2>
+        <b-modal v-model="modal" title="Test">A Test</b-modal>
+        <b-btn @click="modal = true">Show Modal</b-btn>
+        <h2>Tabs</h2>
+        <b-tab :titles="{test: 'Custom text test'}">
+            <template v-slot:test="">
+                <h2>Content of Tab 1</h2>
+            </template>
+            <template v-slot:test2="">
+                <h2>Content of Tab 2</h2>
+            </template>
+        </b-tab>
+    </b-container>
+</template>
+
+<script>
+
+</script>
---
+++ playground/src/main.js
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
+import Vue from 'vue'
+import App from './App.vue'
+
+import { BAccordionItem, BAlert, BBadge, BButton, BCheck, BCollapse, BContainer, BInput, BModal, BPagination, BRange, BSelect, BSpinner, BTab, BTable, BToast } from 'bootstrap5-vue'
+
+Vue.component('b-container', BContainer)
+Vue.component('b-table', BTable)
+Vue.component('b-button', BButton)
+Vue.component('b-btn', BButton)
+Vue.component('b-badge', BBadge)
+Vue.component('b-alert', BAlert)
+Vue.component('b-select', BSelect)
+Vue.component('b-input', BInput)
+Vue.component('b-check', BCheck)
+Vue.component('b-range', BRange)
+Vue.component('b-toast', BToast)
+Vue.component('b-modal', BModal)
+Vue.component('b-spinner', BSpinner)
+Vue.component('b-pagination', BPagination)
+Vue.component('b-tab', BTab)
+Vue.component('b-accordion-item', BAccordionItem)
+Vue.component('b-collapse', BCollapse)
+
+new Vue({
+    render: h => h(App)
+}).$mount('#app')
